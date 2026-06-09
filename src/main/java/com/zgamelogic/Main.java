954ceb46bd4cd218205f0e15bd78ba49b328286f
zabory <benshabowski@gmail.com>
2026-06-08 22:03:01 -0500

    Add initial project structure with configuration files and main application class

---
+++ src/main/java/com/zgamelogic/Main.java
@@ -0,0 +1,19 @@
+package com.zgamelogic;
+
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Main {
+    static void main(String[] args) throws IOException {
+        Terminal term = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(term);
+        WindowBasedTextGUI gui = new MultiWindowTextGUI(screen);
+        screen.startScreen();
+    }
+}
